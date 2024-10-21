89cd45332f1f1a960f3d37d16a427ac6fc5a861a
emiliamaldo <m.emiliamaldo@gmail.com>
2024-10-21 11:32:13 -0300

    Agrego estructura de recinto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddRecintoComponent } from './recinto/components/add-recinto/add-recinto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddRecintoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/recinto/direccion.interface.ts
@@ -0,0 +1,8 @@
+export interface Direccion {
+  id?: number,
+  calle: string,
+  numero: number,
+  ciudad: string,
+  codigoPostal: string,
+  pais: string
+}
