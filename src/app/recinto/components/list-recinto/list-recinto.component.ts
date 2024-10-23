ce739eeba36fec0308d18c47c26da0542ea8faed
emiliamaldo <m.emiliamaldo@gmail.com>
2024-10-23 13:12:39 -0300

    get de recintos

--- src/app/recinto/components/list-recinto/list-recinto.component.ts
+++ src/app/recinto/components/list-recinto/list-recinto.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { RecintoService } from './../../../services/recintos.service';
+import { Component, inject, OnInit } from '@angular/core';
 import { Recinto } from '../../recinto.interface';
 import { AddRecintoComponent } from '../add-recinto/add-recinto.component';
 
@@ -9,8 +10,29 @@ import { AddRecintoComponent } from '../add-recinto/add-recinto.component';
   templateUrl: './list-recinto.component.html',
   styleUrl: './list-recinto.component.css'
 })
-export class ListRecintoComponent {
+export class ListRecintoComponent implements OnInit{
+
+  recintosService= inject(RecintoService);
+
   listaRecintos: Recinto[] = [];
+
+  ngOnInit(): void {
+    this.listarRecintos();
+  }
+
+  listarRecintos()
+  {
+    this.recintosService.getRecintos().subscribe(
+      {
+        next: (recintos: Recinto[])=>{
+          this.listaRecintos= recintos;
+        },
+        error: (err)=> {
+          console.error('Error al levantar recintos:', err);
+        }
+      }
+    )
+  }
 
   addListaRecinto (recinto: Recinto){
     this.listaRecintos.push(recinto);
