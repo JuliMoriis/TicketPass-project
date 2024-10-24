ec830df71ecd533ccb71d798e90902924c2d55a6
JuliMoriis <julymoris2@gmail.com>
2024-10-24 14:59:26 -0300

    server y json de Eventos

---
+++ src/app/services/evento.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Evento } from '../evento/interfaces/evento.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root', // Asegúrate de que el servicio esté disponible en toda la aplicación
+  })
+
+  export class EventoService {
+    urlBase: string = 'http://localhost:3000/eventos'
+    constructor (private http: HttpClient ){};
+
+    getEventos() : Observable <Evento[]>{
+        return this.http.get<Evento[]>(this.urlBase);
+    }
+
+    postEvento (evento: Evento){
+     return this.http.post<Evento>(this.urlBase, evento);
+
+    }
+
+  }
+
+
+
