d85c89e53ff816f5c73ccfea715f3385ac407176
emiliamaldo <m.emiliamaldo@gmail.com>
2024-10-25 12:26:57 -0300

    componente compra

---
+++ src/app/compra/components/list-compra/list-compra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-compra',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-compra.component.html',
+  styleUrl: './list-compra.component.css'
+})
+export class ListCompraComponent {
+
+}
