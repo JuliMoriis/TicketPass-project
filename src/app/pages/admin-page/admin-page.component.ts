72f9dfadda5150c7091f2ff34f44351e8f7e66aa
emiliamaldo <m.emiliamaldo@gmail.com>
2024-10-28 08:54:52 -0300

    arreglo rutas

--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { VerEventoComponent } from '../../evento/components/ver-evento/ver-evento.component';
 
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterModule],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
-
-  constructor(private router: Router) {} // Asegúrate de importar Router
 
 }
