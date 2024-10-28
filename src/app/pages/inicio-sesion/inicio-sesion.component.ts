36e26c9a905162c94e8b3bfcbf0a8999e5d0e1a9
emiliamaldo <m.emiliamaldo@gmail.com>
2024-10-28 11:17:21 -0300

    form inicio sesion

--- src/app/pages/inicio-sesion/inicio-sesion.component.ts
+++ src/app/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../usuario/interfaces/usuario.interface';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -10,11 +12,75 @@ import { RouterModule } from '@angular/router';
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.css'
 })
-export class InicioSesionComponent {
+export class InicioSesionComponent implements OnInit{
+
+  constructor (private router: Router){}
+
+  userService = inject(UsuarioService);
+
+  ngOnInit(): void {
+    this.getusersDB();
+  }
+
+  usuariosDB: { nombreUsuarioDB: string, contraseniaDB: string, tipoDB: number }[] = [];
+
+  submitted = false;
+
+  getusersDB() {
+    this.userService.getUsuarios().subscribe(
+      {
+        next: (users: Usuario[]) => {
+          users.forEach(user => {
+            this.usuariosDB.push({
+              nombreUsuarioDB: user.nombreUsuario, // Asegúrate de que "user.user" y "user.password" sean los nombres correctos en tu modelo
+              contraseniaDB: user.contrasenia,
+              tipoDB: user.tipo
+            });
+          });
+        },
+        error: (e: Error) => {
+          console.log(e.message);
+        }
+      }
+    );
+  }
+
+  exiteUsuario(nombreUsuarioForm: string, contraseniaForm: string): boolean {
+    return this.usuariosDB.some(userDB =>
+      userDB.nombreUsuarioDB === nombreUsuarioForm && userDB.contraseniaDB === contraseniaForm
+    );
+  }
+
+  encontrarTipo(nombreUsuarioForm: string): number {
+    let tipoEncontrado = 0;
+    this.usuariosDB.forEach(user => {
+      if (user.nombreUsuarioDB == nombreUsuarioForm)
+      {
+        tipoEncontrado= user.tipoDB;
+      }
+    });
+
+    return tipoEncontrado;
+  }
 
   iniciarSesion(form: NgForm){
-    
 
+    if (form.valid) {
+
+      if (this.exiteUsuario(form.value.user, form.value.passwordIS)) {
+        let tipoEncontrado= this.encontrarTipo(form.value.usuarioForm);
+        console.log(tipoEncontrado);
+        if (tipoEncontrado == 2){
+          this.router.navigate(["admin"]);
+        }
+        else if (tipoEncontrado == 1){
+          alert("hola cliente")
+        }
+
+      } else {
+        alert("Usuario o contraseña incorrecta");
+      }
+    }
   }
 
 }
