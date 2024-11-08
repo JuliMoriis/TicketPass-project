55ccc26e6ef5ea355829e7d95cd50318463b0791
emiliamaldo <m.emiliamaldo@gmail.com>
2024-11-08 19:01:10 -0300

    alta editado usuario a medias

---
+++ src/app/usuario/pages/page-ver-perfil/page-ver-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-ver-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-ver-perfil.component.html',
+  styleUrl: './page-ver-perfil.component.css'
+})
+export class PageVerPerfilComponent {
+
+}
