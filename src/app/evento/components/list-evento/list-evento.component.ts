0ccb07fe11f8aeeb22016dd7e24f524dfd8d0ea8
laradb10 <laradbmdp@gmail.com>
2024-11-08 11:20:14 -0300

    boton editar

--- src/app/evento/components/list-evento/list-evento.component.ts
+++ src/app/evento/components/list-evento/list-evento.component.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-list-evento',
   standalone: true,
-  imports: [AddEventoComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './list-evento.component.html',
   styleUrl: './list-evento.component.css'
 })
