4f0d96f0cc211a88382ee4ba3251968b2c790a41
emiliamaldo <m.emiliamaldo@gmail.com>
2024-11-08 20:32:05 -0300

    filtrar recintos

--- src/app/recinto/pages/page-recintos-list/page-recintos-list.component.ts
+++ src/app/recinto/pages/page-recintos-list/page-recintos-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ListRecintoComponent } from '../../components/list-recinto/list-recinto.component';
 import { RouterLink, RouterModule } from '@angular/router';
+import { FiltrarRecintoComponent } from '../../components/filtrar-recintos/filtrar-recintos.component';
 
 @Component({
   selector: 'app-page-recintos-list',
   standalone: true,
-  imports: [ListRecintoComponent, RouterModule],
+  imports: [ListRecintoComponent, RouterModule, FiltrarRecintoComponent],
   templateUrl: './page-recintos-list.component.html',
   styleUrl: './page-recintos-list.component.css'
 })
